58c26d58822837625e65b0815003f4b0d7828d4c
Bilguun Ochirbat <bilguun0203@gmail.com>
2019-08-13 18:48:38 +0800

    Added info

--- convert_ebooks_2_images.py
+++ convert_ebooks_2_images.py
@@ -8,9 +8,11 @@ pdf_dir = "./dataset/ebooks"
 os.system("mkdir -p ./dataset/ebooks")
 os.system("mkdir -p ./dataset/images")
 
-for pdf_file in os.listdir(pdf_dir):
+pdfs = os.listdir(pdf_dir)
+
+for i, pdf_file in enumerate(pdfs):
     if pdf_file.endswith(".pdf"):
-        print(pdf_file)
+        print('[{}/{}]'.format(i+1, len(pdfs)), pdf_file)
         try:
             pages = convert_from_path(os.path.join(pdf_dir, pdf_file), 300, output_folder='./dataset/images', output_file=str(pdf_file)+str(time.time()), fmt='jpg', thread_count=4)
         except KeyboardInterrupt:

ca9d1f15fa2821463984fd492bdd91f7f4da7cf7
Bilguun Ochirbat <bilguun0203@gmail.com>
2019-08-13 17:28:22 +0800

    Fixed memory issue

--- collect_arxiv_papers.py
+++ collect_arxiv_papers.py
@@ -3,6 +3,6 @@ import arxiv
 topics = ['quantum', 'deep learning', 'machine learning', 'mathematics']
 
 for topic in topics:
-    result = arxiv.query(query=topic, max_chunk_results=10, iterative=True)
+    result = arxiv.query(query=topic, max_chunk_results=100, iterative=True)
     for paper in result():
         arxiv.download(paper, dirpath="./dataset/ebooks", slugify=arxiv.slugify)
--- convert_ebooks_2_images.py
+++ convert_ebooks_2_images.py
@@ -1,6 +1,7 @@
 import os
+import time
 from pdf2image import convert_from_path
-from PyPDF2 import PdfFileReader
+#from PyPDF2 import PdfFileReader
 
 pdf_dir = "./dataset/ebooks"
 
@@ -11,11 +12,12 @@ for pdf_file in os.listdir(pdf_dir):
     if pdf_file.endswith(".pdf"):
         print(pdf_file)
         try:
-            pages = convert_from_path(os.path.join(pdf_dir, pdf_file), 300)
-            for page in pages:
-                output = os.path.join('./dataset', 'images', str(page)+".jpg")
-                page.save(output, 'JPEG')
-        except:
+            pages = convert_from_path(os.path.join(pdf_dir, pdf_file), 300, output_folder='./dataset/images', output_file=str(pdf_file)+str(time.time()), fmt='jpg', thread_count=4)
+        except KeyboardInterrupt:
+            print('Interrupting...')
+            break
+        except Exception as e:
+            print('Error:', str(e))
             pass
         #print(pages)
 
